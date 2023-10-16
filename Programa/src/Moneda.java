699fe6e41874107c5f64819faa9df75ed1e6878a
Rhussu <wzaratesolar@gmail.com>
2023-10-15 21:26:56 -0300

    Implementada interfaz Comparable

--- Programa/src/Moneda.java
+++ Programa/src/Moneda.java
@@ -1,8 +1,20 @@
-abstract class Moneda{
-    public Moneda(){}
+abstract class Moneda implements Comparable<Moneda> {
+    public Moneda() {
 
-    public Moneda getSerie(){
+    }
+
+    public Moneda getSerie() {
         return this;
     }
+
     public abstract int getValor();
+
+    @Override
+    public int compareTo(Moneda moneda) {
+        if(this.getValor() == moneda.getValor()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
