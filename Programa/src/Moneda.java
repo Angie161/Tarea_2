2b99c50c9bd9be01dd8b64aecf64df61aecaabd6
Rhussu <wzaratesolar@gmail.com>
2023-10-15 22:42:49 -0300

    Agregada la documentación de la clase abstracta Moneda

--- Programa/src/Moneda.java
+++ Programa/src/Moneda.java
@@ -1,14 +1,35 @@
-abstract class Moneda implements Comparable<Moneda> {
-    public Moneda() {
+/**
+ * Esta clase abstracta es la base (superclase) para los distintas clases que representan una moneda de un valor espesifico.
+ */
+public abstract class Moneda implements Comparable<Moneda> {
 
+    /**
+     * Constructor por defecto.
+     */
+    public Moneda() {
     }
 
+    /**
+     * Es como un método getter para la serie de la moneda.
+     * 
+     * @return La dirección en memoria del objeto, esto representa su serie.
+     */
     public Moneda getSerie() {
         return this;
     }
 
+    /**
+     * Método abstracto para obtener el valor de la moneda, las subsclases lo deben sobreescribir.
+     */
     public abstract int getValor();
 
+    /**
+     * Método compareTo() sobreescrito de la interfaz Comaparable y con el objetivo de obtener información al comparar dos monedas.
+     * 
+     * @param moneda La moneda con la que se quiera comparar el valor.
+     * 
+     * @return Devuelve 1 si el valor de las monedas comparadas es el mismo y 0 si sus valores son distintos.
+     */
     @Override
     public int compareTo(Moneda moneda) {
         if(this.getValor() == moneda.getValor()) {
