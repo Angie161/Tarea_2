af3cad6b89642e8103d924fdd2bf20f8214faf48
DoubleYou <146099765+Rhussu@users.noreply.github.com>
2023-10-17 12:20:08 -0300

    Ajuste en un parametro del constructor de Comprador

--- Programa/src/Comprador.java
+++ Programa/src/Comprador.java
@@ -8,14 +8,14 @@ public class Comprador {
     /**
      * Este es un constructor personalizado y realiza la operación de comprar un producto en el expendedor.
      *
-     * @param moneda                    Una Moneda con lo que se pagará.
-     * @param cualProducto              El tipo de producto que se desea comprar.
-     * @param expendedor                El Expendedor en el cual se va a comprar.
+     * @param moneda                     Una Moneda con lo que se pagará.
+     * @param cualProducto               El tipo de producto que se desea comprar representado por un entero. 0 = Coca cola, 1 = Sprite, 2 = Fanta, 3 = Snikers y 4 = Super8.
+     * @param expendedor                 El Expendedor en el cual se va a comprar.
      * @throws PagoIncorrectoException   En caso de que la Moneda sea nula.
      * @throws PagoInsuficienteException Si el valor de las monedas es menor al del producto.
      * @throws NoHayProductoException    En caso de que no quede el producto que se intenta comprar.
      */
-    public Comprador(Moneda moneda, TipoProducto cualProducto, Expendedor expendedor) throws PagoIncorrectoException, NoHayProductoException, PagoInsuficienteException {
+    public Comprador(Moneda moneda, int cualProducto, Expendedor expendedor) throws PagoIncorrectoException, NoHayProductoException, PagoInsuficienteException {
         try {
             sonido = expendedor.comprarProducto(moneda, cualProducto).consumir();
         } catch(PagoIncorrectoException | NoHayProductoException | PagoInsuficienteException e) {
