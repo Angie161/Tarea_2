86ba14e2d73695a831f5a73b84f87d10010178eb
Rhussu <wzaratesolar@gmail.com>
2023-10-19 00:22:24 -0300

    Agregada la documentación de la clase Producto

--- MavenProg/src/main/java/CocaCola.java
+++ MavenProg/src/main/java/CocaCola.java
@@ -1,7 +1,22 @@
-public class CocaCola extends Bebida{
-    public CocaCola (int serie){
+/**
+ * Esta es una subclase de Bebida y representa una cocacola.
+ */
+public class CocaCola extends Bebida {
+
+    /**
+     * Constructor personalizado de Cocacola que permite asignarle un número de serie.
+     * 
+     * @param serie Es el número que representa la serie del objeto.
+     */
+    public CocaCola (int serie) {
         super(serie);
     }
+
+    /**
+     * Este método es para representarse asi mismo al consumirse.
+     * 
+     * @return Un string diciendo que es una cocacola.
+     */
     public String consumir() {
         return "cocacola";
     }
