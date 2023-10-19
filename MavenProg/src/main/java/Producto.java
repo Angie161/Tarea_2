6e81366c616539161c009e4db122dd83f9fff0cd
Rhussu <wzaratesolar@gmail.com>
2023-10-19 00:22:54 -0300

    Agregada la documentación de la clase Producto

--- MavenProg/src/main/java/Producto.java
+++ MavenProg/src/main/java/Producto.java
@@ -1,5 +1,17 @@
+/**
+ * Esta es la supersuperclase de todos los productos del programa (Cocacola, Sprite, Fanta, Snickers, Super8) y es para dar la base a estos.
+ */
 public abstract class Producto {
-    public Producto(){}
 
+    /**
+     * Constructor por defecto.
+     */
+    public Producto() {
+
+    }
+
+    /**
+     * Método abstracto que será utilizado por todas las subsubclases para representar lo que son.
+     */
     public abstract String consumir();
 }
